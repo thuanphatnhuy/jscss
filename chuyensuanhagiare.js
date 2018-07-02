b5e9f009c908b608162e683454e396d121802feb
thuanphatnhuy <thuanphatnhuy@gmail.com>
2018-07-02 15:55:58 +0700

    Add files via upload

---
+++ chuyensuanhagiare.js
@@ -0,0 +1,18 @@
+
+function FSAddOriginalLink(){
+    var body_element = document.getElementsByTagName('body')[0];
+    var selection;
+    selection = window.getSelection();
+    var pagelink = "<br/><br/>Chúc mừng bạn đã copy bài viết của chúng tôi https://chuyensuanhagiare.com : <a href='https://chuyensuanhagiare.com'>https://chuyensuanhagiare.com"+document.location.pathname+"</a>";
+    var copytext = pagelink;
+    var newdiv = document.createElement('div');
+    newdiv.style.position='absolute';
+    newdiv.style.left='-99999px';
+    body_element.appendChild(newdiv);
+    newdiv.innerHTML = copytext;
+    selection.selectAllChildren(newdiv);
+    window.setTimeout(function() {
+        body_element.removeChild(newdiv);
+    },0);
+}
+document.oncopy = FSAddOriginalLink;
