415d4fb9f0cf35f17e24eabf851acb905a394886
thuanphatnhuy <thuanphatnhuy@gmail.com>
2018-07-04 22:25:19 +0700

    Update thuanphatnhuy.js

--- thuanphatnhuy.js
+++ thuanphatnhuy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>Bạn muốn copy phải vào thuanphatnhuy.com mới có thể copy được : <a href='https://thuanphatnhuy.com/'>https://thuanphatnhuy.com/"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>Trang web đã có bản quyền thuanphatnhuy.com mới có thể copy được : <a href='https://thuanphatnhuy.com/'>https://thuanphatnhuy.com/"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
